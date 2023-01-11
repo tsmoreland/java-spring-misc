4f19ad4f38b51fb37c379309d8ec177abf8e9eb5
Terry Moreland <terry.s.moreland@gmail.com>
2023-01-11 09:57:08 +0000

    initial project from start.spring.io + updates to gitignore

---
+++ jdbc-console-demo/src/main/java/com/tsmoreland/spring/jdbcconsoledemo/JdbcconsoledemoApplication.java
@@ -0,0 +1,13 @@
+package com.tsmoreland.spring.jdbcconsoledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcconsoledemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcconsoledemoApplication.class, args);
+	}
+
+}
---
+++ jdbc-console-demo/src/test/java/com/tsmoreland/spring/jdbcconsoledemo/JdbcconsoledemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsmoreland.spring.jdbcconsoledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcconsoledemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
