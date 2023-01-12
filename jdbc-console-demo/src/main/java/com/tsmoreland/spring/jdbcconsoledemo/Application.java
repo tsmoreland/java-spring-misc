b0f4ad7231056ff00bbd04d3601d31d6800f9cb2
Terry Moreland <tsmoreland@live.com>
2023-01-12 00:33:14 +0000

    rename application to Application.java

--- jdbc-console-demo/src/main/java/com/tsmoreland/spring/jdbcconsoledemo/Application.java
+++ jdbc-console-demo/src/main/java/com/tsmoreland/spring/jdbcconsoledemo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JdbcconsoledemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JdbcconsoledemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
