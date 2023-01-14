b1ff238b7e6b0a880fb4b5cf7f3dcfa233adbc10
Terry Moreland <tsmoreland@live.com>
2023-01-14 20:31:45 +0000

    correct method names to match java naming conventions

--- jdbcdemo/application/src/main/java/com/tsmoreland/spring/jdbcdemo/application/contracts/persistence/CategoryRepository.java
+++ jdbcdemo/application/src/main/java/com/tsmoreland/spring/jdbcdemo/application/contracts/persistence/CategoryRepository.java
@@ -6,6 +6,6 @@ import com.tsmoreland.spring.jdbcdemo.domain.entities.Category;
 import java.util.List;
 
 public interface CategoryRepository extends Repository<Category> {
-    Page<Category> GetPage(int pageNumber, int pageSize, boolean includeEvents);
-    List<Category> GetAll(boolean includeEvents);
+    Page<Category> getPage(int pageNumber, int pageSize, boolean includeEvents);
+    List<Category> getAll(boolean includeEvents);
 }
