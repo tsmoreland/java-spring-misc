115a02cd5aa8b138776adb7081c35840ebb9eccf
Terry Moreland <tsmoreland@live.com>
2023-01-14 19:57:55 +0000

    add base repository interface

---
+++ jdbcdemo/application/src/main/java/com/tsmoreland/spring/jdbcdemo/contracts/persistence/Repository.java
@@ -0,0 +1,16 @@
+package com.tsmoreland.spring.jdbcdemo.contracts.persistence;
+
+import com.tsmoreland.spring.jdbcdemo.domain.common.Page;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface Repository<T> {
+    Optional<T> GetById(UUID id);
+    List<T> GetAll();
+    Page<T> GetPage(int pageNumber, int pageSize);
+    T Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
