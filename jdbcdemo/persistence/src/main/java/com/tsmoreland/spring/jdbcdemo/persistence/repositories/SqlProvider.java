3d7fe43f67fe9034251dc7448a70b963c7ebef73
Terry Moreland <tsmoreland@live.com>
2023-01-20 00:20:32 +0000

    tweaks to provider interface, initial implementation underway

--- jdbcdemo/persistence/src/main/java/com/tsmoreland/spring/jdbcdemo/persistence/repositories/SqlProvider.java
+++ jdbcdemo/persistence/src/main/java/com/tsmoreland/spring/jdbcdemo/persistence/repositories/SqlProvider.java
@@ -4,6 +4,7 @@ import com.tsmoreland.spring.jdbcdemo.domain.common.Page;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -12,9 +13,8 @@ public interface SqlProvider<T> {
     String getById();
     String getAll();
     String getPage();
-    String add();
-    PreparedStatement prepareAddStatement(Connection connection, T entity);
-    PreparedStatement prepareUpdateStatement(Connection connection, T entity);
+    PreparedStatement prepareAddStatement(Connection connection, T entity) throws SQLException;
+    PreparedStatement prepareUpdateStatement(Connection connection, T entity) throws SQLException;
     String delete();
     String getTotalCount();
 }
